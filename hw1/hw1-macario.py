6b040d1e10581b056a646e42eb5ea2e14b68a960
davmacario <dav.macario@gmail.com>
2023-09-11 19:51:33 -0500

    Update hw1 code

--- hw1/hw1-macario.py
+++ hw1/hw1-macario.py
@@ -4,25 +4,67 @@ import random
 from queue import PriorityQueue
 
 
+class Packet():
+    def __init__(self, id: int, t_arr: float):
+        """
+        Packet
+        ---
+        Class modeling packets being processed by the queuing system.
+
+        ### Input parameters
+        - id: packet ID (integer unique identifier) # FIXME: maybe useless (ID corresponds to position in list?)
+        - t_arr: arrival time
+        """
+        self.t_arr = t_arr
+        self.id = id
+
+
+"""
+Event scheduling
+
+Event syntax:
+- arrival:
+- end of service:
+"""
+
+
 class Queue():
-    def __init__(self, n_servers: int, arr_rate: float, serv_rate: float, queue_len=None):
+    def __init__(self, n_servers: int, arr_rate: float, serv_rate: float, queue_len: int | None = None):
         """
         Queue
         ---
         This class models M/M/m queues
         """
         self.n_servers = n_servers
+        self.being_served = [-1] * self.n_servers
         self.arr_rate = arr_rate
         self.serv_rate = serv_rate
         self.n_packets = 0
+        self.packets_list = []
+        self.pkt_id = 0
+
         self.serv_occupied = [0] * n_servers
         self.queue_len = queue_len
+
+        self.events = []
 
         self.last_event_time = 0
         # List containing (time, n_packets) values (sampled at n_packets variations)
         self.N_t = []
         # Cumulative sum of N_t*DeltaT, where DeltaT is the time distance between N_t variations
-        self.ut = 0
+        self.ut = 0     # Maybe not needed
+        # Avg. service time - FIXME: time avg or not???
+        self.T_t = 0
+
+        ############
+        # N. arrivals eval. at time t - elem (t, n)
+        self.n_arr = 0
+        self.n_arr_time = []
+        # N. departures eval. at time t - elem (t, n)
+        self.n_dep = 0
+        self.n_dep_time = []
+        # Time of arrivals
+        self.t_arr = []
 
     def arrival(self, curr_time: float, event_set: PriorityQueue):
         """
@@ -39,9 +81,39 @@ class Queue():
         the next scheduled arrival.
         """
         if self.queue_len is None:
+            # Add packet to the queue
             self.n_packets += 1
+            self.packets_list.append(Packet(id=self.pkt_id, t_arr=curr_time))
 
-    def departure(self, time: float, event_set: PriorityQueue):
+            self.n_arr += 1
+            self.n_arr_time.append((curr_time, self.n_arr))
+            self.t_arr.append(curr_time)
+
+            self.Nt.append((curr_time, self.n_packets - 1))
+            self.ut += (self.n_packets - 1) * \
+                (curr_time - self.last_event_time)
+
+            if self.n_packets <= self.n_servers:
+                # If free servers, start services
+                serv_index = self.being_served.index(-1)
+                # Add the id of the packet being served
+                self.being_served[serv_index] = self.pkt_id
+                serv_time = random.expovariate(self.serv_rate)
+
+                new_event = (curr_time + serv_time,
+                             ["end of serv", serv_index, self.pkt_id])
+                event_set.put(new_event)
+
+            self.pkt_id += 1
+
+            # Schedule next arrival
+            inter_arr_time = random.expovariate(self.arr_rate)
+            new_event = (curr_time + inter_arr_time, ["arrival"])
+
+            # Update the last event time
+            self.last_event_time = curr_time
+
+    def departure(self, curr_time: float, event_set: PriorityQueue, args: list):
         """
         departure
         ---
@@ -50,7 +122,146 @@ class Queue():
         in service.
 
         Input parameters:
-        - time: current time, extracted from the event in the FES.
+        - curr_time: current time, extracted from the event in the FES.
         - event_set: (priority queue) future event set (for scheduling). Used to place
         the next scheduled departure.
+        - args: list of parameters contained in the event (extracted from event set)
         """
+        server_id = args[1]
+        packet_id = args[2]
+
+        # Extract packet from list 'self.packets_list'
+        i = 0
+        while i < len(self.packets_list) and self.packets_list[i].id != packet_id:
+            i += 1
+        if self.packets_list[i].id != packet_id:
+            raise ValueError(
+                f"The packet with the provided id ({packet_id}) is not in the queue!")
+
+        finished_pkt = self.packets_list.pop(i)
+
+        # Update measurements
+        self.n_dep += 1
+        self.n_dep_time.appen((curr_time, self.n_dep))
+
+        self.N_t.append((curr_time, self.n_packets))
+        self.ut += self.n_packets * (curr_time - self.last_event_time)
+
+        self.n_packets -= 1
+
+        # Make server idle
+        assert self.being_served[server_id] == packet_id, f"The server is not serving the right client according to 'self.being_served'"
+        self.being_served[server_id] = -1
+
+        # If there are still elements in the queue, try to serve a new pkt
+        if self.n_packets >= self.n_servers:
+            # Find packet to be served
+            j = 0
+            while j < len(self.packets_list) and self.packets_list[j].id not in self.being_served:
+                j += 1
+            if self.packets_list[j].id not in self.being_served:
+                raise ValueError(
+                    f"Should be able to serve a new packet, but no good packet was found!")
+
+            # Add the id of the packet being served
+            self.being_served[server_id] = j
+            serv_time = random.expovariate(self.serv_rate)
+
+            new_event = (curr_time + serv_time,
+                         ["end of serv", server_id, j])
+            event_set.put(new_event)
+
+    def print_results(self, sim_time: float):
+        """
+        print_results
+        ---
+        Evaluate and print the results at the end of the simulation.
+
+        The returned plots are:
+        - N_t vs. t (time average of the number of packets in the system)
+        - T_t vs. t (time average of the system delay)
+
+        Additionally, the function evaluates the empirical average arrival rate 
+        via Little's theorem (over the entire run), and compares it with the 
+        actual arrival rate used.
+        """
+        # Time average of the number of packets:
+        N_tau = []      # Format: (t, n)
+        i = -1
+        j = -1
+        t_curr = 0
+        while i < len(self.n_arr_time) and j < len(self.n_dep_time):
+            # Find lowest
+            if self.n_arr_time[i + 1][0] - t_curr < self.n_dep_time[i + 1][0] - t_curr:
+                # Next is an arrival
+                i += 1
+                curr_alpha = self.n_arr_time[i][1]
+                curr_beta = self.n_arr_time[j][1] if j >= 0 else 0
+                t_curr = self.n_arr_time[i][0]
+                N_tau.append((t_curr, curr_alpha - curr_beta))
+            elif self.n_arr_time[i + 1][0] - t_curr > self.n_dep_time[i + 1][0] - t_curr:
+                # Next is a departure
+                j += 1
+                curr_alpha = self.n_arr_time[i][1]
+                curr_beta = self.n_arr_time[j][1]
+                t_curr = self.n_dep_time[j][0]
+                N_tau.append((t_curr, curr_alpha - curr_beta))
+            else:
+                # Two events at the same time - should not happen
+                # The number of packets does not change
+                pass
+
+        avg_N = []
+        Ndeltat = 0
+        for i in range(1, len(N_tau)):
+            Ndeltat += N_tau[i - 1][1] * (N_tau[i][0] - N_tau[i - 1][0])
+            avg_N.append((N_tau[i][0], Ndeltat/N_tau[i][0]))
+        avg_N_np = np.array(avg_N)
+
+        plt.figure()
+        plt.plot(avg_N_np[:, 0], avg_N_np[:, 1])
+        plt.title("Time average of the number of packets in the queue")
+        plt.xlabel("t")
+        plt.ylabel("N_tau")
+        plt.grid()
+        plt.show()
+
+
+def main(sim_time: float, arr_rate: float, serv_rate: float, n_serv: int):
+    """
+    Main program loop.
+
+    # Input parameters
+    - sim_time: total simulation time (seconds)
+    - arr_rate: lambda (in 1/s)
+    - serv_rate: mu (in 1/s)
+    - n_serv: number of servers (the queue is M/M/n_serv)
+    """
+
+    event_set = PriorityQueue()
+
+    queue = Queue(n_serv, arr_rate, serv_rate)
+
+    time = 0
+
+    # Schedule an arrival to start simulation
+    event_set.put((time, ["arrival"]))
+
+    while time < sim_time:
+        # Extract next event
+        (time, event_args) = event_set.get()
+
+        if event_args[0] == "arrival":
+            # queue.events.append((time, "arrival"))
+            queue.arrival(time, event_set)
+        elif event_args[0] == "end of serv":
+            # queue.events.append((time, "end of serv"))
+            queue.departure(time, event_set, event_args)
+
+    # Perform evaluations and plots
+    queue.print_results(time)
+
+
+if __name__ == "__main__":
+    # Perform simulation
+    main(3600, 5, 10, 1)
